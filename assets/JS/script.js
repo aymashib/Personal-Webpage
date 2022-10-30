634a76d6e978bb8011813eb77b1d944a3074e172
Ayman <aymashiban@gmail.com>
2022-10-30 12:42:18 +0100

    30-10

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,6 @@
 /* DOM*/
-line1_element = document.getElementById("line1");
-line2_element = document.getElementById("line2");
-line3_element = document.getElementById("line3");
+line1_element = document.getElementById("#line1");
+line2_element = document.getElementById(".line2");
+line3_element = document.getElementById(".line3");
 
-
-
+line1_element
